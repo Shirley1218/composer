b6912b9210edf010077a34523c4f1bd1ffc1d4d5
Shirley1218 <zhouxian94@hotmail.com>
2016-11-10 20:37:49 -0500

    counters tested

--- datapath.v
+++ datapath.v
@@ -4,6 +4,7 @@
 /*                                                            */
 /*                                                            */
 /*  Last edit: Nov 10    set bmp : whole note 2s; 4 bars 8 s  */
+/*                        delay counters simulation passed    */
 /**************************************************************/
 
 
@@ -17,7 +18,10 @@ module datapath(input CLOCK_50,menu_enable,list_enable,note_enable,song_list_ena
 	/////////clock dividers	/////////////////////						
 	clock_5MHz CLK5(CLOCK_50,clk_5M);	
 	clock_16Hz CLK16(clk_5M,clk_16);
-					
+	wire [2:0]insert_delay_out;
+	wire [6:0]play_delay_out;
+
+	
 	////////vga modules	////////////////////////
 	menu M1();  				//VGA display module  -- draw menu
 	modeSelect M2(); 			//VGA display module  -- draw menu list
@@ -70,7 +74,7 @@ module datapath(input CLOCK_50,menu_enable,list_enable,note_enable,song_list_ena
 				.resetn(resetn),
 				.record(1'b0),					//make no change
 				.erase(1'b0),
-				.play(PlayEnable),
+				.play(playEnable),
 				//.ps2_clock(ps2_clock),
 				.ps2_in(keyboard_input),
 				.hsync(VGA_HS),
@@ -83,26 +87,26 @@ module datapath(input CLOCK_50,menu_enable,list_enable,note_enable,song_list_ena
 					
 					
 					
-	///////when user enters a note, wait until note is drawn or its sound is played///////
+	///////when user enters a note, wait until note is drawn or its sound is played/////// tested
 	
 		
 	defparam InsertDelay.n = 3;   //  0.5 second -> 2Hz
 	updownCounter InsertDelay(clk_16,reset,InsertEnable,1'b1,insert_delay_out);
-	assign insert_delay_done = (insert_delay_out == 3'b111) ? 1'b1 : 1'b0;
+	assign insert_delay_done = (insert_delay_out == 3'b110) ? 1'b1 : 1'b0;
 	
 	
-	///////when user decides to play all notes, wait until all nodes are played///////
+	///////when user decides to play all notes, wait until all nodes are played/////// tested
 		
 	defparam PlayDelay.n = 7; ///   4 bar -> 8 second
-	updownCounter PlayDelay(clk_16,reset,PlayEnable,1'b1,Play_delay_out);
-	assign play_done = (play_delay_out == 7'b1111111) ? 1'b1 : 1'b0;
+	updownCounter PlayDelay(clk_16,reset,playEnable,1'b1,play_delay_out);
+	assign play_done = (play_delay_out == 7'b1111110) ? 1'b1 : 1'b0;
 
 endmodule
 
 module updownCounter(clock,reset,enable,up_down,Q);
 	parameter n = 8;
 	input clock,reset,enable,up_down;
-	output reg [n-1:0]Q;
+	output reg [n-1:0]Q = 0;
 
 	always@(posedge clock)
 	begin
@@ -110,6 +114,7 @@ module updownCounter(clock,reset,enable,up_down,Q);
 			Q <= 0;
 		else if(enable)
 			Q <= Q + (up_down ? 1 : -1);
+		
 	end
 		
 endmodule
